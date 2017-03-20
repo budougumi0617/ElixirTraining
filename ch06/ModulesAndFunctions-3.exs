478275e96d09510dc26eb843969fc9d1ac26b430
budougumi0617 <budougumi0617@gmail.com>
2017-03-20 23:45:13 +0900

    :recycle: Refactoring call.

--- ch06/ModulesAndFunctions-3.exs
+++ ch06/ModulesAndFunctions-3.exs
@@ -1,7 +1,7 @@
 defmodule Times do
     def double(n), do: n * 2
     def triple(n), do: n * 3
-    def quadruple(n), do: Times.double(Times.double(n))
+    def quadruple(n), do: double double n
 end
 
 IO.puts Times.quadruple(3)
