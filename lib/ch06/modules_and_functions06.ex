b491a8a2dcdffcb8b3ff2c409f03c3e4633c3aa5
budougumi0617 <budougumi0617@gmail.com>
2017-04-02 21:28:56 +0900

    :recycle: Refactoring ch06ex06.

--- lib/ch06/modules_and_functions06.ex
+++ lib/ch06/modules_and_functions06.ex
@@ -1,27 +1,19 @@
 defmodule Chop do
-    def guess(actual, range) do
-        min..max = range
-        case max do
-            e when e == actual -> e
-            _ -> _guess(actual, range, div(min + max, 2))
-        end
+    def guess(actual, min..max) do
+        e = div(min + max, 2)
+        IO.puts "Is it #{e}"
+        _guess(actual, min..max, e)
     end
 
-    def _guess(actual, range, estimate \\ 0) 
-
     def _guess(actual, _, estimate) when actual == estimate do
-        IO.puts "Is it #{estimate}"
         estimate
     end
 
-    def _guess(actual, range, estimate) when actual < estimate do
-        min..max = range
-        IO.puts "Is it #{estimate}"
-        _guess(actual, min..estimate-1, div(min + estimate-1, 2))
+    def _guess(actual, min.._, estimate) when actual < estimate do
+        guess(actual, min..estimate-1)
     end
-    def _guess(actual, range, estimate) when estimate < actual do
-        min..max = range
-        IO.puts "Is it #{estimate}"
-        _guess(actual, estimate+1..max, div(estimate+1 + max, 2))
+
+    def _guess(actual, _..max, estimate) when estimate < actual do
+        guess(actual, estimate+1..max)
     end
 end
