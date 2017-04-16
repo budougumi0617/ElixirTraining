984c7696ab6c9ef13191e05fa49786de48339bd0
budougumi0617 <budougumi0617@gmail.com>
2017-04-16 22:13:38 +0900

    :memo: Add test.

--- test/ch07/lists_and_recursion02_test.exs
+++ test/ch07/lists_and_recursion02_test.exs
@@ -5,6 +5,7 @@ defmodule MyList02Test do
     assert MyList02.max([1,2,3,4,3]) == 4
     assert MyList02.max([1,2,30,4,3]) == 30
     assert MyList02.max([1000,30,40,320]) == 1000
+    assert MyList02.max([-1, -2, -5, -3]) == -1
   end
 
 end
