0c9dfdee300974dcd26886b62b07224ed35fdd53
budougumi0617 <budougumi0617@gmail.com>
2017-04-16 21:24:27 +0900

    :recycle: Use const value.

--- lib/ch07/lists_and_recursion03.ex
+++ lib/ch07/lists_and_recursion03.ex
@@ -1,6 +1,8 @@
 defmodule MyList03 do
-    def caesar([head | []], n) when head + n < 122, do: [head + n]
-    def caesar([head | []], n), do: [head + rem(n, 122) - 26]
-    def caesar([head | tail], n) when head + n < 122, do: [head + n | caesar(tail, n)]
-    def caesar([head | tail], n),  do: [head + rem(n, 122) - 26| caesar(tail, n)]
+    @z 122
+    @charnumber 26
+    def caesar([head | []], n) when head + n < @z, do: [head + n]
+    def caesar([head | []], n), do: [head + rem(n, @z) - @charnumber]
+    def caesar([head | tail], n) when head + n < @z, do: [head + n | caesar(tail, n)]
+    def caesar([head | tail], n),  do: [head + rem(n, @z) - @charnumber| caesar(tail, n)]
 end
