6f85dabde430ada659acd568798703258fd41323
budougumi0617 <budougumi0617@gmail.com>
2017-04-16 23:58:57 +0900

    :bug: Fix bug.

--- lib/ch07/lists_and_recursion03.ex
+++ lib/ch07/lists_and_recursion03.ex
@@ -1,8 +1,7 @@
 defmodule MyList03 do
     @z 122
-    @charnumber 26
-    def caesar([head | []], n) when head + n < @z, do: [head + n]
-    def caesar([head | []], n), do: [head + rem(n, @z) - @charnumber]
-    def caesar([head | tail], n) when head + n < @z, do: [head + n | caesar(tail, n)]
-    def caesar([head | tail], n),  do: [head + rem(n, @z) - @charnumber| caesar(tail, n)]
+    @a 96
+    def caesar([], _ ), do: []
+    def caesar([head | tail], n) when head + rem(n, @z) <= @z, do: [head + rem(n, @z) | caesar(tail, n)]
+    def caesar([head | tail], n),  do: [rem(head + rem(n, @z), @z) + @a| caesar(tail, n)]
 end
--- test/ch07/lists_and_recursion03_test.exs
+++ test/ch07/lists_and_recursion03_test.exs
@@ -3,6 +3,10 @@ defmodule MyList03Test do
 
   test "Noused accumulator" do
     assert MyList03.caesar('ryvkve', 13) == 'elixir'
+    assert MyList03.caesar('abz', 3) == 'dec'
+    assert MyList03.caesar('abc', 122) == 'abc'
+    assert MyList03.caesar('abz', 125) == 'dec'
+    assert MyList03.caesar('z', 122) == 'z'
   end
 
 end
