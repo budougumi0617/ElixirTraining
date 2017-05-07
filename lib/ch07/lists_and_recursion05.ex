75cb0f9d4f51930baa53c2a09061500a92144255
budougumi0617 <budougumi0617@gmail.com>
2017-05-07 21:26:14 +0900

    :+1: Add ch10ex05 files.

---
+++ lib/ch07/lists_and_recursion05.ex
@@ -0,0 +1,7 @@
+defmodule MyList05 do
+    def all?(list, f), do: []
+    def each(list, f), do: []
+    def filter(list, f), do: []
+    def split(list, f), do: []
+    def take(list, f), do: []
+end
