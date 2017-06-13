e26f20267e9aef87be704c98983b789b1bb91be8
budougumi0617 <budougumi0617@gmail.com>
2017-06-14 07:52:22 +0900

    :+1: Same strings are not anagram.

--- lib/ch11/strings_and_binaries02.ex
+++ lib/ch11/strings_and_binaries02.ex
@@ -1,3 +1,4 @@
 defmodule MyStrings02 do
+    def anagram?(s1, s2) when s1 == s2, do: false
     def anagram?(s1, s2), do: s1 -- s2 == [] and s2 -- s1 == []
 end
--- test/ch11/strings_and_binaries02_test.exs
+++ test/ch11/strings_and_binaries02_test.exs
@@ -2,10 +2,12 @@ defmodule MyString02Test do
   use ExUnit.Case
 
   test "Strings are anagram." do
-
-  assert MyStrings02.anagram?('test', 'stte') == true
-  assert MyStrings02.anagram?('test', 'foo') == false
-  assert MyStrings02.anagram?('test', 'tttest') == false
+    assert MyStrings02.anagram?('test', 'stte') == true
+    assert MyStrings02.anagram?('test', 'foo') == false
+    assert MyStrings02.anagram?('test', 'tttest') == false
   end
 
+  test "Same stirings are not anagram." do
+    assert MyStrings02.anagram?('test', 'test') == false
+  end
 end
