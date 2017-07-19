f8180b88bcc2fd54d85b60766a7d0ebcd12831d1
budougumi0617 <budougumi0617@gmail.com>
2017-07-19 09:58:43 +0900

    :+1: Initial commit issues.CLI.

---
+++ apps/issues/lib/issues/cli.ex
@@ -0,0 +1,25 @@
+defmodule Issues.CLI do
+
+  @default_count 4
+
+  @doc """
+  Handle the command line parsing and the dispach to
+  the verious functions that end up generating a
+  table of the last _n_ issues in a github project.
+  """
+
+  def run(argv) do
+    parse_args(argv)
+  end
+
+  @doc """
+  `argv` can be -h or --help, which returns :help.
+
+  Otherwise it is a github user name, project name, and (optionally)
+  the number of entries to format.
+  Return a tuple of `{user, project, count}`, or `:help` if help was given.
+  """
+
+  def parse_args(argv) do
+  end
+end
