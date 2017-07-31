cb90d516a30354f396987135d0c0659e23659587
budougumi0617 <budougumi0617@gmail.com>
2017-07-31 14:02:41 +0900

    :+1: Fix typo, create test file.

--- apps/issues/lib/issues/table_formatter.ex
+++ apps/issues/lib/issues/table_formatter.ex
@@ -1,5 +1,5 @@
 defmodule Issues.TableFormatter do
-    import Enum, only: [ each: 2, map: 2, map_join: 4, max: 1 ]
+    import Enum, only: [ each: 2, map: 2, map_join: 3, max: 1 ]
 
     def print_table_for_columns(rows, headers) do
         with data_by_columns = split_into_columns(rows, headers),
@@ -10,37 +10,38 @@ defmodule Issues.TableFormatter do
             IO.puts(separator(column_widths))
             puts_in_columns(data_by_columns, format)
         end
+    end
 
-        def split_into_columns(rows, headers) do
-            for header <- headers do
-                for row <- rows, do: printable(row[header])
-            end
-        end
-
-        def printable(str) when is_binary(str), do: str
-        def printable(str), do: to_string(str)
-
-        def widths_of(columns) do
-            for column <- columns, do: column |> map(&String.length/1) |> max
-        end
-
-        def format_for(column_widths) do
-            map_join(column_widths, " | ", fn width -> "~-#{width}s" end) <> "~n"
-        end
-
-        def separator(column_widths) do
-            map_join(column_widths, "-+-"m fn width -> List.duplicate("-", width) end)
-        end
-
-        def puts_in_columns(data_by_columns) do
-            data_by_columns
-            |> List.zip
-            |> map(&Taple.to_list/1)
-            |> each(&puts_one_line_in_columns(&1, format))
-        end
-
-        def puts_one_line_in_columns (fields, format) do
-            :io.format(format, fields)
+    def split_into_columns(rows, headers) do
+        for header <- headers do
+            for row <- rows, do: printable(row[header])
         end
     end
+
+    def printable(str) when is_binary(str), do: str
+    def printable(str), do: to_string(str)
+
+    def widths_of(columns) do
+        for column <- columns, do: column |> map(&String.length/1) |> max
+    end
+
+    def format_for(column_widths) do
+        map_join(column_widths, " | ", fn width -> "~-#{width}s" end) <> "~n"
+    end
+
+    def separator(column_widths) do
+        map_join(column_widths, "-+-", fn width -> List.duplicate("-", width) end)
+    end
+
+    def puts_in_columns(data_by_columns, format) do
+        data_by_columns
+        |> List.zip
+        |> map(&Taple.to_list/1)
+        |> each(&puts_one_line_in_columns(&1, format))
+    end
+
+    def puts_one_line_in_columns(fields, format) do
+        :io.format(format, fields)
+    end
+
 end
