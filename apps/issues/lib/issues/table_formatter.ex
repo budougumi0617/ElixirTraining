0f9746ae0745a9d649f06bdec02a861dcfd8721d
budougumi0617 <budougumi0617@gmail.com>
2017-08-08 13:50:32 +0900

    :+1: Add comments.

--- apps/issues/lib/issues/table_formatter.ex
+++ apps/issues/lib/issues/table_formatter.ex
@@ -1,47 +1,69 @@
 defmodule Issues.TableFormatter do
-    import Enum, only: [ each: 2, map: 2, map_join: 3, max: 1 ]
+  import Enum, only: [ each: 2, map: 2, map_join: 3, max: 1 ]
 
-    def print_table_for_columns(rows, headers) do
-        with data_by_columns = split_into_columns(rows, headers),
-             column_widths   = widths_of(data_by_columns),
-             format          = format_for(column_widths)
-        do
-            puts_one_line_in_columns(headers, format)
-            IO.puts(separator(column_widths))
-            puts_in_columns(data_by_columns, format)
-        end
+  @doc """
+  Takes a list of row data, where each row is a Map, and a list of
+  headers. Prints a table to STDOUT of the data from each row
+  identified by each header. That is, each header identifies a column,
+  and those column are extracted and printd from the rows.0
+
+  We caluculate the width of each column to fit the longest element
+  in that column.
+  """
+  def print_table_for_columns(rows, headers) do
+    with data_by_columns = split_into_columns(rows, headers),
+      column_widths   = widths_of(data_by_columns),
+      format          = format_for(column_widths)
+    do
+      puts_one_line_in_columns(headers, format)
+      IO.puts(separator(column_widths))
+      puts_in_columns(data_by_columns, format)
     end
+  end
 
-    def split_into_columns(rows, headers) do
-        for header <- headers do
-            for row <- rows, do: printable(row[header])
-        end
+  @doc """
+  Given a list of rows, where each row contains a keyed list
+  of columns, return a list containing lists of the data in
+  each column. The `headers`parameter contains the
+  list of columns to extract
+
+  ## Example
+
+      iex> list = [Enum.into([{"a", "1"},{"b", "2"},{"c", "3"}, %{}]),
+      ...>         Enum.into([{"a", "4"},{"b", "5"},{"c", "6"}, %{}])]
+      iex> Issues.TableFormatter.split_into_columns(list, [ "a", "b", "c" ])
+      [ ["1", "4"], ["2", "5"], ["3", "6"] ]
+  """
+  def split_into_columns(rows, headers) do
+    for header <- headers do
+      for row <- rows, do: printable(row[header])
     end
+  end
 
-    def printable(str) when is_binary(str), do: str
-    def printable(str), do: to_string(str)
+  def printable(str) when is_binary(str), do: str
+  def printable(str), do: to_string(str)
 
-    def widths_of(columns) do
-        for column <- columns, do: column |> map(&String.length/1) |> max
-    end
+  def widths_of(columns) do
+    for column <- columns, do: column |> map(&String.length/1) |> max
+  end
 
-    def format_for(column_widths) do
-        map_join(column_widths, " | ", fn width -> "~-#{width}s" end) <> "~n"
-    end
+  def format_for(column_widths) do
+    map_join(column_widths, " | ", fn width -> "~-#{width}s" end) <> "~n"
+  end
 
-    def separator(column_widths) do
-        map_join(column_widths, "-+-", fn width -> List.duplicate("-", width) end)
-    end
+  def separator(column_widths) do
+    map_join(column_widths, "-+-", fn width -> List.duplicate("-", width) end)
+  end
 
-    def puts_in_columns(data_by_columns, format) do
-        data_by_columns
-        |> List.zip
-        |> map(&Tuple.to_list/1)
-        |> each(&puts_one_line_in_columns(&1, format))
-    end
+  def puts_in_columns(data_by_columns, format) do
+    data_by_columns
+    |> List.zip
+    |> map(&Tuple.to_list/1)
+    |> each(&puts_one_line_in_columns(&1, format))
+  end
 
-    def puts_one_line_in_columns(fields, format) do
-        :io.format(format, fields)
-    end
+  def puts_one_line_in_columns(fields, format) do
+    :io.format(format, fields)
+  end
 
 end
