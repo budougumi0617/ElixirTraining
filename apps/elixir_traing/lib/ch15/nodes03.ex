f9f90afbaad2c3b7607dd3ba6177914b9bb43ee5
budougumi0617 <budougumi0617@gmail.com>
2017-10-21 11:22:02 +0900

    :+1: Add Nodes03.

--- apps/elixir_traing/lib/ch15/nodes03.ex
+++ apps/elixir_traing/lib/ch15/nodes03.ex
@@ -5,24 +5,27 @@ defmodule Ticker do
 
   def start do
     pid = spawn(__MODULE__, :generator, [[]])
+    # @nameという名前でpidを登録しておく。
     :global.register_name(@name, pid)
   end
 
   def register(client_pid) do
-    send :grobal.whereis_name(@name), { :register, client_pid }
+    # @nameという名前で登録されているpidに対してメソッドを呼び出す。
+    send :global.whereis_name(@name), { :register, client_pid }
   end
 
   def generator(clients) do
     receive do
       { :register, pid } ->
         IO.puts "registering #{inspect pid}"
-        generator([pid|clients])
+        generator(Enum.reverse([pid|clients]))
     after
-        @interval ->
-          IO.puts "tick"
-        Enum.each clients, fn client  ->
-          send client, { :tick }
-        end
+      # タイムアウトの設定
+      @interval ->
+        IO.puts "tick"
+      Enum.each clients, fn client  ->
+        send client, { :tick }
+      end
       generator(clients)
     end
   end
@@ -38,8 +41,8 @@ defmodule Client do
   def receiver do
     receive do
       { :tick  } ->
-        IO.puts "took in client"
-        receiver
+        IO.puts "took in client #{inspect self()}"
+        receiver()
     end
   end
 end
