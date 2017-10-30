4f0ca491ee36aaf456b0f72d5ef1f02ba9aa551e
budougumi0617 <budougumi0617@gmail.com>
2017-10-30 23:45:08 +0900

    W.I.P. Add sample answer.

--- apps/elixir_traing/lib/ch15/nodes04.ex
+++ apps/elixir_traing/lib/ch15/nodes04.ex
@@ -1,79 +1,41 @@
-defmodule Ticker do
+defmodule Ticker04 do
 
   def start do
-    pid = spawn(__MODULE__, :generator, [[]])
-    # @nameという名前でpidを登録しておく。
-    :global.register_name(@name, pid)
+    spawn(__MODULE__, :generator, [])
+  end
+
+  def add_client do
+    pid = spawn __MODULE__, :receiver, []
+    register pid
+  end
+
+  def ticker(next_client \\ self) do
+    :global.register_name @name, self
+    receive do
+      { :register, new_client } ->
+        IO.puts "registering #{inspect new_client}"
+        send new_client, { :tick, next_client }
+        receiver(new_client)
+      after @interval ->
+        IO.puts "tick"
+        send next_client, { :tick }
+        receiver(next_client)
+    end
+  end
+
+  def receiver(next_client \\ self) do
+    receive do
+      { :tick, new_client } ->
+        IO.puts "first tock in #{inspect self}, sending a tick to #{inspect new_client} in 2s"
+        ticker(new_client)
+      { :tick } ->
+        IO.puts "tock in #{inspect self}, sending a tick to #{inspect next_client} in 2s"
+        ticker(next_client)
+    end
   end
 
   def register(client_pid) do
-    # @nameという名前で登録されているpidに対してメソッドを呼び出す。
     send :global.whereis_name(@name), { :register, client_pid }
   end
-
-  def generator(clients) do
-    receive do
-      { :register, pid } ->
-        IO.puts "registering #{inspect pid}"
-        generator(Enum.reverse([pid|clients]))
-    after
-      @interval ->
-        IO.puts "tick"
-        case clients do
-          [ head | tail ] ->
-            send head, { :tick }
-            generator( tail ++ [ head ]) # Poor performance.
-          [ client ] ->
-            send client, { :tick }
-            generator(client)
-          [] ->
-            generator([])
-        end
-    end
-  end
 end
 
-defmodule Client do
-  @interval 2000 # 2 seconds
-  @head     :head
-  @next     :next
-
-  def start_link do
-    Agent.start_link(fn -> %{} end, name: __MODULE__)
-  end
-
-  def head do
-    case Agent.get(__MODULE__, fn map -> map[@head] end) do
-      nil -> Agent.update(__MODULE__, fn map -> Map.put(map, @head, self()) end)
-      head -> head
-    end
-  end
-
-  def next do
-    case Agent.get(__MODULE__, fn map -> map[@next] end) do
-      nil -> Agent.update(__MODULE__, fn map -> Map.put(map, @next, self()) end)
-      next ->
-        Agent.update(__MODULE__, fn map -> Map.put(map, @next, self()) end)
-        next
-    end
-  end
-
-  def start do
-    pid = spawn(__MODULE__, :receiver, [])
-    # Ticker.register(pid)
-    # 最初のClientはheadとして登録しておく？
-    # 「次に送信PID」みたいな場所に登録する
-    # 登録してあるのが自分だったら、自分より新しいnodeがないので、headに通知する。そうするとリングになる？
-    # iex(1)> :global.whereis_name('tst')
-    # :undefined
-    next = :global.get_update(:next)
-  end
-
-  def receiver do
-    receive do
-      { :tick  } ->
-        IO.puts "took in client #{inspect self()}"
-        receiver()
-    end
-  end
-end
