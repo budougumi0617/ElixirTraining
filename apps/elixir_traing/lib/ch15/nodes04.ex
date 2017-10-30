90779965457e361f5668500a36937f13c0c129bf
budougumi0617 <budougumi0617@gmail.com>
2017-10-31 00:17:56 +0900

    :memo: W.I.P. Add comments.

--- apps/elixir_traing/lib/ch15/nodes04.ex
+++ apps/elixir_traing/lib/ch15/nodes04.ex
@@ -13,13 +13,13 @@ defmodule Ticker04 do
   end
 
   def ticker(next_client \\ self()) do
-    :global.register_name @name, self()
+    :global.register_name @name, self() # 自分を登録しておいてから:registerが来るのを待機する。
     receive do
       { :register, new_client } ->
         IO.puts "registering #{inspect new_client}"
         send new_client, { :tick, next_client }
         receiver(new_client)
-      after @interval ->
+      after @interval -> # :registerが呼ばれなかった場合はsendしてreceiverで待機する。
         IO.puts "tick"
         send next_client, { :tick }
         receiver(next_client)
@@ -28,15 +28,16 @@ defmodule Ticker04 do
 
   def receiver(next_client \\ self()) do
     receive do
-      { :tick, new_client } -> # 引数有りで呼び出された場合は引数PIDにsendする。
+      { :tick, new_client } -> # 引数有りで呼び出された場合
         IO.puts "first tock in #{inspect self()}, sending a tick to #{inspect new_client} in 2s"
         ticker(new_client)
-      { :tick } -> # 引数なしだった場合は自分にsendする。
+      { :tick } -> # 引数なしだった場合は自己ループを繰り返す。
         IO.puts "tock in #{inspect self()}, sending a tick to #{inspect next_client} in 2s"
         ticker(next_client)
     end
   end
 
+  # 親プロセスにメッセージを送る。
   def register(client_pid) do
     send :global.whereis_name(@name), { :register, client_pid }
   end
