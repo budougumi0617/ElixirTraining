c077f17b78a4979c8fc12f849b0ee7fc63ce206b
budougumi0617 <budougumi0617@gmail.com>
2017-11-15 21:22:39 +0900

    :+1: Add result.

--- apps/sequence/lib/myserver/otp_server.ex
+++ apps/sequence/lib/myserver/otp_server.ex
@@ -49,4 +49,39 @@ end
 # pop/0           push/1          start_link/1
 # iex(1)> OtpServer.OtpServer.pop
 # 123
-
+# iex(2)> OtpServer.OtpServer.pop
+# state : {{:badmatch, []}, [{OtpServer.OtpServer, :handle_call, 3, [file: 'lib/myserver/otp_server.ex', line: 23]}, {:gen_server, :try_handle_call, 4, [file: 'gen_server.erl', line: 636]}, {:gen_server, :handle_msg, 6, [file: 'gen_server.erl', line: 665]}, {:proc_lib, :init_p_do_apply, 3, [file: 'proc_lib.erl', line: 247]}]}
+# reason : []
+# ** (exit) exited in: GenServer.call(OtpServer.OtpServer, :pop, 5000)
+#    ** (EXIT) an exception was raised:
+#        ** (MatchError) no match of right hand side value: []
+#            (sequence) lib/myserver/otp_server.ex:23: OtpServer.OtpServer.handle_call/3
+#            (stdlib) gen_server.erl:636: :gen_server.try_handle_call/4
+#            (stdlib) gen_server.erl:665: :gen_server.handle_msg/6
+#            (stdlib) proc_lib.erl:247: :proc_lib.init_p_do_apply/3
+#    (elixir) lib/gen_server.ex:774: GenServer.call/3
+# iex(2)>
+# 21:18:57.626 [error] GenServer OtpServer.OtpServer terminating
+# ** (MatchError) no match of right hand side value: []
+#     (sequence) lib/myserver/otp_server.ex:23: OtpServer.OtpServer.handle_call/3
+#     (stdlib) gen_server.erl:636: :gen_server.try_handle_call/4
+#     (stdlib) gen_server.erl:665: :gen_server.handle_msg/6
+#     (stdlib) proc_lib.erl:247: :proc_lib.init_p_do_apply/3
+# Last message (from #PID<0.122.0>): :pop
+# State: []
+# Client #PID<0.122.0> is alive
+#     (stdlib) gen.erl:169: :gen.do_call/4
+#     (elixir) lib/gen_server.ex:771: GenServer.call/3
+#     (stdlib) erl_eval.erl:670: :erl_eval.do_apply/6
+#     (elixir) src/elixir.erl:239: :elixir.eval_forms/4
+#     (iex) lib/iex/evaluator.ex:219: IEx.Evaluator.handle_eval/5
+#     (iex) lib/iex/evaluator.ex:200: IEx.Evaluator.do_eval/3
+#     (iex) lib/iex/evaluator.ex:178: IEx.Evaluator.eval/3
+#     (iex) lib/iex/evaluator.ex:77: IEx.Evaluator.loop/1
+# 
+# 
+# nil
+# iex(3)> OtpServer.OtpServer.push 10
+# :ok
+# iex(4)> OtpServer.OtpServer.pop
+# 10
