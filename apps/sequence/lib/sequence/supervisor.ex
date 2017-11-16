9789404ba774b131ae22538288d54d724b7b01ce
budougumi0617 <budougumi0617@gmail.com>
2017-11-16 09:47:14 +0900

    :+1: Add Supervisor.

---
+++ apps/sequence/lib/sequence/supervisor.ex
@@ -0,0 +1,18 @@
+defmodule OtpServer.Supervisor do
+  use Supervisor
+  def start_link(initial_queue) do
+    result= {:ok, sup} = Supervisor.start_link(__MODULE__, [initial_queue])
+    start_workers(sup, initial_queue)
+    result
+  end
+  def start_workers(sup, [initial_queue]) do
+    # スタッシュワーカーを開始
+    {:ok, stash} =
+      Supervisor.start_child(sup, worker(OtpServer.Stash, [initial_queue]))
+    # そして実際の stack サーバのスーパーバイザを開始
+    Supervisor.start_child(sup, supervisor(OtpServer.SubSupervisor, [stash]))
+  end
+  def init(_) do
+    supervise [], strategy: :one_for_one
+  end
+end
