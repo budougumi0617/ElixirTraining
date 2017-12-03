0328532462c7a7936c285d6530eaa9afcebb21b6
budougumi0617 <budougumi0617@gmail.com>
2017-12-03 21:40:46 +0900

    :+1: Update version.

--- apps/otp_applications_3/mix.exs
+++ apps/otp_applications_3/mix.exs
@@ -4,7 +4,7 @@ defmodule OtpApplications3.Mixfile do
   def project do
     [
       app: :otp_applications_3,
-      version: "0.0.1",
+      version: "0.0.3",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
