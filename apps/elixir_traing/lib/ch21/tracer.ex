70f27f2d31ea3dfd7c3a9276222ce76a6dcec58a
budougumi0617 <budougumi0617@gmail.com>
2017-12-24 12:21:23 +0900

    :sparkles: Update version

--- apps/elixir_traing/lib/ch21/tracer.ex
+++ apps/elixir_traing/lib/ch21/tracer.ex
@@ -1,7 +1,20 @@
 defmodule Tracer do
-  defmacro def(definition, do: _content) do
-    IO.inspect definition
-    quote do: {}
+  def dump_args(args) do
+    args |> Enum.map(&inspect/1) |> Enum.join(", ")
+  end
+
+  def dump_defn(name, args) do
+    "#{name}(#{dump_args(args)})"
+  end
+
+  defmacro def(definition={name, _, args}, do: content) do
+    quote do
+      Kernel.def(unquote(definition)) do
+        IO.puts "==> call:   #{Tracer.dump_defn(unquote(name), unquote(args))}"
+        result = unquote(content)
+        IO.puts "<== result: #{result}"
+      end
+    end
   end
 end
 
