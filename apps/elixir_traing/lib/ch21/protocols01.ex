399b50b38143f0be671d97ebb2b6746f23445c41
budougumi0617 <budougumi0617@gmail.com>
2017-12-24 23:02:05 +0900

    :+1: Modify example

--- apps/elixir_traing/lib/ch21/protocols01.ex
+++ apps/elixir_traing/lib/ch21/protocols01.ex
@@ -12,19 +12,23 @@ defimpl Protocols01, for: [BitString, List] do
 
   def shift_char(c, shift) do
     cond do
-      c+ rem(shift, @z) <= @z -> c+ rem(shift, @z)
-      true -> rem(c+ rem(shift, @z), @z) + @a
+      c + rem(shift, @z) <= @z -> c+ rem(shift, @z)
+      true -> rem(c + rem(shift, @z), @z) + @a
     end
   end
 end
 
-IO.puts Protocols01.encrypt("test abc", 1)
+IO.puts Protocols01.encrypt("test abcz", 1)
 
 # Result
+# iex lib/ch21/protocols01.ex
 # Erlang/OTP 20 [erts-9.1.2] [source] [64-bit] [smp:4:4] [ds:4:4:10] [async-threads:10] [hipe] [kernel-poll:false] [dtrace]
 #
-# uftu!bcd
+# uftu!bcda
 # Interactive Elixir (1.5.2) - press Ctrl+C to exit (type h() ENTER for help)
+# iex(1)>
 
 # References
 # apps/elixir_traing/lib/ch07/lists_and_recursion03.ex
+
+
