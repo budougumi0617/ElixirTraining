1f0ea648d2735737d006253a38e1d6204ff57f89
budougumi0617 <budougumi0617@gmail.com>
2018-01-09 13:54:26 +0900

    :art: Done Protocols02

--- apps/elixir_traing/lib/ch22/protocols02.ex
+++ apps/elixir_traing/lib/ch22/protocols02.ex
@@ -7,7 +7,7 @@ defimpl Protocols02, for: [BitString, List] do
   @z 122
   @a 96
 
-  def encrypt(string, shift), do: string |> to_charlist |> Enum.map(&(shift_char(&1, shift)))
+  def encrypt(string, shift), do: string |> to_charlist |> Enum.map(&(shift_char(&1, shift))) |> List.to_string
   def rot13(string), do: encrypt(string, 13)
 
   def shift_char(c, shift) do
@@ -19,28 +19,25 @@ defimpl Protocols02, for: [BitString, List] do
 end
 
 defmodule SearchRot do
-  @file_name "words.txt"
-
-  def rotation_exists?(word, words) do
-    rotated = Protocols02.rot13(word)
-    MapSet.member?(words, rotated)
-  end
-
-  def filter_results(word, words, acc) do
-    case rotation_exists?(word, words) do
-      true -> [word | acc]
-      false -> acc
-    end
-  end
+  @file_name "2of12.txt"
+  # @file_name "words.txt"
 
   def searh_rot13() do
-    words = @file_name
-    |> File.stream!
-    |> Stream.map(&String.trim(&1))
-    |> MapSet.new
+    words_set = @file_name
+                |> File.stream!
+                |> Stream.map(&String.trim(&1))
+                |> MapSet.new
 
-    words
-    |> Enum.reduce([], fn(word, acc)-> filter_results(word, words, acc) end)
+    Enum.flat_map(words_set, fn word -> 
+      rot_word = Protocols02.rot13(word)
+      # IO.inspect words_set
+      # IO.inspect "#{word} -> #{rot_word}"
+      case MapSet.member?(words_set, rot_word) do
+        true ->  
+        ["#{word} -> #{rot_word}"]
+        false -> []
+      end
+    end)
   end
 
 end
@@ -48,14 +45,14 @@ end
 IO.inspect SearchRot.searh_rot13
 
 # Result
-# iex lib/ch21/protocols01.ex
-# Erlang/OTP 20 [erts-9.1.2] [source] [64-bit] [smp:4:4] [ds:4:4:10] [async-threads:10] [hipe] [kernel-poll:false] [dtrace]
-#
-# uftu!bcda
-# Interactive Elixir (1.5.2) - press Ctrl+C to exit (type h() ENTER for help)
-# iex(1)>
-
-# References
-# apps/elixir_traing/lib/ch07/lists_and_recursion03.ex
-
+# $ elixir protocols02.ex
+# ["i -> v", "g -> t", "a -> n", "s -> f", "balk -> onyx", "n -> a", "m -> z",
+#  "nowhere -> abjurer", "ah -> nu", "q -> d", "r -> e", "d -> q", "j -> w",
+#  "p -> c", "fur -> she", "nu -> ah", "nun -> aha", "she -> fur", "ant -> nag",
+#  "oho -> bub", "gel -> try", "onyx -> balk", "envy -> rail", "ova -> bin",
+#  "x -> k", "v -> i", "o -> b", "t -> g", "z -> m", "sync -> flap", "l -> y",
+#  "bar -> one", "b -> o", "k -> x", "e -> r", "f -> s", "y -> l", "h -> u",
+#  "nag -> ant", "u -> h", "bub -> oho", "gnat -> tang", "try -> gel",
+#  "crag -> pent", "be -> or", "tang -> gnat", "flap -> sync", "rail -> envy",
+#  "pent -> crag", "abjurer -> nowhere", ...]
 
